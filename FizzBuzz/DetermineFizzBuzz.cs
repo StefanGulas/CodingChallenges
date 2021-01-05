6152c0e4e623a37fcc2d506603b7a02016fdd108
Stefan <stefan.gulas@gmail.com>
2021-01-05 20:25:46 +0100

    Optimized Solution 1 with

---
+++ FizzBuzz/DetermineFizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz
+{
+  public class DetermineFizzBuzz
+  {
+    public void Determine(int totalNumber, int fizz, int buzz)
+    {
+      for (int i = 1; i < totalNumber; i++)
+      {
+        if (i % fizz != 0 && i % buzz != 0) System.Console.Write(i);
+        if (i % fizz == 0) System.Console.Write("Fizz");
+        if (i % buzz == 0) System.Console.Write("Buzz");
+        System.Console.WriteLine();
+      }
+    }
+  }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,28 +6,33 @@ namespace FizzBuzz
   {
     static void Main(string[] args)
     {
-      for (int i = 0; i < 100; i++)
-      {
-        if (i % 3 == 0)
-        {
-          if (i % 5 == 0)
-          {
-            Console.WriteLine("FizzBuzz");
-            continue;
-          }
-          Console.WriteLine("Fizz");
-          continue;
-        }
-        else if (i % 5 == 0)
-        {
-          Console.WriteLine("Buzz");
-          continue;
-        }
-        Console.WriteLine(i);
-      }
+
+      var fizzBuzz = new DetermineFizzBuzz();
+
+      fizzBuzz.Determine(100, 3, 5);
+      
+      //for (int i = 0; i < 100; i++)
+      //{
+      //  if (i % 3 == 0)
+      //  {
+      //    if (i % 5 == 0)
+      //    {
+      //      Console.WriteLine("FizzBuzz");
+      //      continue;
+      //    }
+      //    Console.WriteLine("Fizz");
+      //    continue;
+      //  }
+      //  else if (i % 5 == 0)
+      //  {
+      //    Console.WriteLine("Buzz");
+      //    continue;
+      //  }
+      //  Console.WriteLine(i);
+      //}
       
       
-      Console.WriteLine("Hello World!");
+      //Console.WriteLine("Hello World!");
     }
   }
 }
