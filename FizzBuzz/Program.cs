0a6c06d81df9836286dbe0dcc8b4c7a966b1edee
Stefan <stefan.gulas@gmail.com>
2021-01-05 06:22:04 +0100

    Projektdateien hinzufügen.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 100; i++)
+      {
+        if (i % 3 == 0)
+        {
+          if (i % 5 == 0)
+          {
+            Console.WriteLine("FizzBuzz");
+            continue;
+          }
+          Console.WriteLine("Fizz");
+          continue;
+        }
+        else if (i % 5 == 0)
+        {
+          Console.WriteLine("Buzz");
+          continue;
+        }
+        Console.WriteLine(i);
+      }
+      
+      
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
