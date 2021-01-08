52fb4bac1dcb800597aa263e254fbfe66b8115fd
Stefan <stefan.gulas@gmail.com>
2021-01-08 17:00:38 +0100

    Fibonacci Sequence started

---
+++ CodingChallenges/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges
+{
+  public class Fibonacci
+  {
+    public List<int> CalculateFibonacci(int position)
+    {
+      var fibonacci = new List<int>();
+
+      return fibonacci;
+    }
+  }
+}
---
+++ CodingChallengesTests/FibonacciTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodingChallenges;
+using NUnit.Framework;
+
+namespace CodingChallengesTests
+{
+  public class FibonacciTests
+  {
+    [Test]
+    public void EqualsIfPositionIs8()
+    {
+      var fibonacci = new Fibonacci();
+      var expectedFibonacciList = new List<int>() { 0, 1, 1, 2, 3, 5, 8, 13 };
+      int position = 8;
+      var actualFibonacciList = fibonacci.CalculateFibonacci(position);
+
+      Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
+    }
+  }
+}
