bd4c261352b211c41f0bccd12564d25dda62a3d6
Stefan <stefan.gulas@gmail.com>
2021-01-08 18:55:46 +0100

    Fibonacci working

--- CodingChallenges/Fibonacci.cs
+++ CodingChallenges/Fibonacci.cs
@@ -9,7 +9,20 @@ namespace CodingChallenges
     public List<int> CalculateFibonacci(int position)
     {
       var fibonacci = new List<int>();
-
+      fibonacci.Add(0);
+      if (position == 0) return fibonacci;
+      fibonacci.Add(1);
+      if (position == 1) return fibonacci;
+      fibonacci.Add(1);
+      if (position == 2) return fibonacci;
+      int fibonacciMinus1 = 1;
+      int fibonacciMinus2 = 1;
+      for (int i = 3; i <= position; i++)
+      {
+        fibonacci.Add(fibonacciMinus1 + fibonacciMinus2);
+        fibonacciMinus2 = fibonacciMinus1;
+        fibonacciMinus1 = fibonacci[i];
+      }
       return fibonacci;
     }
   }
--- CodingChallengesTests/FibonacciTests.cs
+++ CodingChallengesTests/FibonacciTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using CodingChallenges;
 using NUnit.Framework;
 
@@ -10,11 +9,51 @@ namespace CodingChallengesTests
   public class FibonacciTests
   {
     [Test]
-    public void EqualsIfPositionIs8()
+    public void Equals0IfPositionIs0()
+    {
+      var fibonacci = new Fibonacci();
+      var expectedFibonacciList = new List<int>() { 0 };
+      int position = 0;
+      var actualFibonacciList = fibonacci.CalculateFibonacci(position);
+
+      Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
+    }
+    [Test]
+    public void Equals01IfPositionIs1()
+    {
+      var fibonacci = new Fibonacci();
+      var expectedFibonacciList = new List<int>() { 0, 1 };
+      int position = 1;
+      var actualFibonacciList = fibonacci.CalculateFibonacci(position);
+
+      Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
+    }
+    [Test]
+    public void Equals011IfPositionIs2()
+    {
+      var fibonacci = new Fibonacci();
+      var expectedFibonacciList = new List<int>() { 0, 1, 1 };
+      int position = 2;
+      var actualFibonacciList = fibonacci.CalculateFibonacci(position);
+
+      Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
+    }
+    [Test]
+    public void Equals0112IfPositionIs3()
+    {
+      var fibonacci = new Fibonacci();
+      var expectedFibonacciList = new List<int>() { 0, 1, 1, 2 };
+      int position = 3;
+      var actualFibonacciList = fibonacci.CalculateFibonacci(position);
+
+      Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
+    }
+    [Test]
+    public void Equals011235813IfPositionIs7()
     {
       var fibonacci = new Fibonacci();
       var expectedFibonacciList = new List<int>() { 0, 1, 1, 2, 3, 5, 8, 13 };
-      int position = 8;
+      int position = 7;
       var actualFibonacciList = fibonacci.CalculateFibonacci(position);
 
       Assert.IsTrue(actualFibonacciList.SequenceEqual(expectedFibonacciList));
