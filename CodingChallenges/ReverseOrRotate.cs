e2aed1d236d4296c4dd2345dc1b5ad9ab76e1007
StefanGulas <stefan.gulas@gmail.com>
2021-04-17 21:07:32 +0200

    Improved but not fully working

--- CodingChallenges/ReverseOrRotate.cs
+++ CodingChallenges/ReverseOrRotate.cs
@@ -6,10 +6,34 @@ namespace CodingChallenges
 {
     public class ReverseOrRotate
     {
-        public static string RevRot(string strng, int sz)
+        public static string RevRot(string inputString, int sz)
         {
-            
-            return "";
+            if (sz <= 0 || inputString == String.Empty || sz > inputString.Length) return "";
+            int sum = 0;
+            foreach (var item in inputString)
+            {
+                sum += item ^ 3;
+            }
+            string [] resultArray = new string[inputString.Length];
+            int j = 0;
+            if(sum % 2 == 0)
+            {
+                for (int i = inputString.Length-1; i >= 0; i--)
+                {
+                    resultArray[j] = Char.ToString(inputString[i]);
+                    j++;
+                }
+            }
+            else
+            {
+                resultArray[inputString.Length - 1] = Char.ToString(inputString[0]);
+                for (int i = 1; i < inputString.Length-2; i++)
+                {
+                    resultArray[j] = Char.ToString(inputString[i]);
+                    j++;
+                }
+            }
+            return string.Join("", resultArray);
         }
     }
 }
